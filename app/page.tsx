3de3ed7e77e267015161c1ad0173153d269bd1fc
25HP2526@nest.edu.mn <25hp2526@admins-iMac.local>
2025-11-27 18:33:05 +0800

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import DescribeUrIllness from "./_components/DescribeUrIllness";
+import MenuBar from "./_components/MenuBar";
+
 export default function Home() {
-  return <div>Health!</div>;
+  return (
+    <div className="bg-gradient-to-br from-[#E3FDF5] to-[#D7FFE8] min-h-screen animate-fadeIn">
+      <MenuBar />
+      <DescribeUrIllness />
+    </div>
+  );
 }
