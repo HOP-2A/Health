73018c8289f284fe920582f2e983ab5e992a2e87
turbold10 <turbold959@gmail.com>
2025-12-04 18:20:10 +0800

    gg

--- app/api/add-medicine/route.ts
+++ app/api/add-medicine/route.ts
@@ -1,38 +1,9 @@
 import { prisma } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
-<<<<<<< HEAD
-export const POST = async (req: NextRequest) => {
-  const body = await req.json();
-  const addMedicine = await prisma.medicine.create({
-    data: {
-      name: body.name,
-      description: body.description,
-      ageLimit: body.ageLimit,
-      price: body.price,
-      stock: body.stock,
-      imageUrls: body.imageUrls,
-      category: body.category,
-    },
-  });
-  console.log(addMedicine, "aoisdaoisdoji");
-  return NextResponse.json(
-    { message: "amjilttai", addMedicine },
-    { status: 200 }
-  );
-};
-export const DELETE = async (req: NextRequest) => {
-  const body = await req.json();
-  const DeleteMedicine = await prisma.medicine.delete({
-    where: {
-      id: body.id,
-    },
-  });
-=======
 export const POST = async (req: Request) => {
   try {
     const body = await req.json();
->>>>>>> 5803c57 (hi)
 
     const addMedicine = await prisma.medicine.create({
       data: {
@@ -53,28 +24,6 @@ export const POST = async (req: Request) => {
     return NextResponse.json({ error: "Failed to create" }, { status: 500 });
   }
 };
-<<<<<<< HEAD
-export const PUT = async (req: NextRequest) => {
-  const body = await req.json();
-  const UpdatedMedicine = await prisma.medicine.update({
-    where: {
-      id: body.id,
-    },
-    data: {
-      name: body.name,
-      description: body.description,
-      ageLimit: body.ageLimit,
-      price: body.price,
-      stock: body.stock,
-      imageUrls: body.imageUrls,
-    },
-  });
-  return NextResponse.json(UpdatedMedicine);
-};
-export const GET = async () => {
-  const getMedicine = await prisma.medicine.findMany();
-  return NextResponse.json(getMedicine);
-=======
 
 export const DELETE = async (req: Request) => {
   try {
@@ -118,5 +67,4 @@ export const GET = async () => {
   } catch (error) {
     return NextResponse.json({ message: "fail" });
   }
->>>>>>> 5803c57 (hi)
 };
