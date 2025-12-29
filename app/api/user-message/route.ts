86a743b0720cc7365bce999a4fca7ab2eeaddebe
NOOBCODER <erkhessw@gmail.com>
2025-12-29 21:34:40 +0800

    P

--- app/api/user-message/route.ts
+++ app/api/user-message/route.ts
@@ -1,3 +1,27 @@
-import { NextRequest } from "next/server";
+import { prisma } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
 
-export const POST = (req: NextRequest) => {};
+export const POST = async (req: NextRequest) => {
+  const body = await req.json();
+  const createdMessage = await prisma.userMessage.create({
+    data: {
+      userId: body.userId,
+      illness: body.illnessId,
+      chat: body.chat,
+      doctorId: body.doctorId,
+    },
+  });
+  return NextResponse.json(createdMessage);
+};
+export const GET = async (
+  req: NextRequest,
+  context: { params: { userId: string } }
+) => {
+  const { userId } = context.params;
+  const messages = await prisma.userMessage.findMany({
+    where: {
+      userId,
+    },
+  });
+  return NextResponse.json(messages);
+};
