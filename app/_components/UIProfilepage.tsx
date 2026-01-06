31ef725ebc99130d6745e4ef9faca8ecc228641d
erkhes9632 <o.erkhes2009@gmail.com>
2026-01-06 19:10:25 +0800

    add

--- app/_components/UIProfilepage.tsx
+++ app/_components/UIProfilepage.tsx
@@ -85,7 +85,7 @@ export default function UIProfilePage() {
     getOrder();
     fetchLikes();
   }, [user]);
-  console.log(user);
+
   return (
     <div className="min-h-screen bg-gradient-to-br ">
       <div className="container mx-auto px-4 py-8 max-w-7xl">
