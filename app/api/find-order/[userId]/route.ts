5fba0e16675d87b2b0b722b1f47fd77093247dbd
25hp5780nest.edu.mn <25hp5780nest.edu.mn@admins-iMac.local>
2026-01-06 02:50:20 -0800

    fixed

--- app/api/find-order/[userId]/route.ts
+++ app/api/find-order/[userId]/route.ts
@@ -1,16 +1,17 @@
 import { prisma } from "@/lib/db";
 import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
+import type { OrderItem, Medicine } from "@prisma/client";
 
 export const GET = async (
   req: NextRequest,
-  context: {
-    params: Promise<{ userId: string }>;
-  }
+  context: { params: Promise<{ userId: string }> }
 ) => {
   const params = await context.params;
   const { userId } = params;
+
   const order = await prisma.order.findFirst({
+<<<<<<< HEAD
     where: {
       userId: userId,
       status: "pending",
@@ -33,4 +34,20 @@ export const GET = async (
     order: order,
   };
   if (order) return NextResponse.json(final);
+=======
+    where: { userId },
+    orderBy: { createdAt: "desc" },
+  });
+
+  let orderedItems: (OrderItem & { medicine: Medicine })[] = [];
+
+  if (order) {
+    orderedItems = await prisma.orderItem.findMany({
+      where: { orderId: order.id },
+      include: { medicine: true },
+    });
+  }
+
+  return NextResponse.json({ order, items: orderedItems });
+>>>>>>> b4eb2e3 (fixed)
 };
