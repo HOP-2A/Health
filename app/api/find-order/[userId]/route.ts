bbcc644109d41fb6b2c6884eb20eeda4816969f1
25hp5780nest.edu.mn <25hp5780nest.edu.mn@admins-iMac.local>
2026-01-07 22:02:29 -0800

    end

--- app/api/find-order/[userId]/route.ts
+++ app/api/find-order/[userId]/route.ts
@@ -11,7 +11,6 @@ export const GET = async (
   const { userId } = params;
 
   const order = await prisma.order.findFirst({
-<<<<<<< HEAD
     where: {
       userId: userId,
       status: "pending",
@@ -34,20 +33,4 @@ export const GET = async (
     order: order,
   };
   if (order) return NextResponse.json(final);
-=======
-    where: { userId },
-    orderBy: { createdAt: "desc" },
-  });
-
-  let orderedItems: (OrderItem & { medicine: Medicine })[] = [];
-
-  if (order) {
-    orderedItems = await prisma.orderItem.findMany({
-      where: { orderId: order.id },
-      include: { medicine: true },
-    });
-  }
-
-  return NextResponse.json({ order, items: orderedItems });
->>>>>>> b4eb2e3 (fixed)
 };
